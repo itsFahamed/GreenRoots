bcbb1bb032cb7544baa4e2d1e5414342dd21ab87
fuadahamed <fuadahamedantar@gmail.com>
2026-05-16 21:11:53 +0600

    migrate auth guard to proxy

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
+export function proxy(request) {
   const { pathname, origin } = request.nextUrl;
   const sessionToken =
     request.cookies.get("better-auth.session_token") ||
